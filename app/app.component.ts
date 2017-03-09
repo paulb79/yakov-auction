60526f785e1898a32cd10c45e9912e126f5ffd21
Paul Brown <paul@redjamjar.net>
2017-03-09 10:47:42 +0000

    initial project structure

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app',
+  templateUrl: 'app.component.html',
+  styleUrls: [ 'app.component.css' ]
+})
+
+export class AppComponent {
+  name: string;
+
+  constructor() {
+    this.name = "Willow";
+  }
+}
---
+++ systemjs.config.js
@@ -0,0 +1,29 @@
+System.config({
+  transpiler: 'typescript',
+  typescriptOptions: {
+    emitDecoratorMetadata : true,
+    target: "es5",
+    module: "commonjs"
+  },
+  map: {
+    app     : 'app',
+    '@angular': 'node_modules/@angular',
+    'rxjs'    : 'node_modules/rxjs'
+  },
+  paths: {
+    'node_modules/@angular/*': 'node_modules/@angular/*/bundles'
+  },
+  meta: {
+    '@angular/*': { 'format' : 'cjs' }
+  },
+  packages: {
+    app                                 : { main : './main.js',
+                                            defaultExtension : 'js' },
+    'rxjs'                              : { main : 'Rx' },
+    '@angular/core'                     : { main : 'core.umd.min.js' },
+    '@angular/common'                   : { main : 'common.umd.min.js' },
+    '@angular/compiler'                 : { main : 'compiler.umd.min.js' },
+    '@angular/platform-browser'         : { main : 'platform-browser.umd.min.js' },
+    '@angular/platform-browser-dynamic' : { main : 'platform-browser-dynamic.umd.min.js' }
+  }
+});
